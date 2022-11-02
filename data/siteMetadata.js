8a50b9a9e0e612991696218e85da6c90b0eb5997
radinka-data <85663548+radinka-data@users.noreply.github.com>
2022-11-02 17:13:28 +0700

    Update siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,8 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Azka Radinka Personal Portfolio',
+  author: 'Azka Radinka',
+  headerTitle: 'Aradinka',
+  description: 'A personal portfolio created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
